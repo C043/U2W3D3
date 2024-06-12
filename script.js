dfd11dd4cd5198b086d95b239ec5322d1e874f39
Mario Fragnito <mariofragnitoph@gmail.com>
2024-06-12 19:45:29 +0200

    Adds comments and fixed some issues

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* classe per i libri */
 class Book {
   constructor(title, price) {
     this.title = title;
@@ -5,54 +6,11 @@ class Book {
   }
 }
 
+/* array per gestire i libri nel carrello */
 const cartBooks = [];
 
-const cartGen = () => {
-  const books = localStorage.getItem("Cart Books");
-  const booksRetrieved = JSON.parse(books);
-  const cartList = document.getElementById("cart-list");
-  cartList.style.width = "300px";
-  const cart = document.getElementById("cart");
-  cart.innerText = ` ${booksRetrieved.length}`;
-  for (let i = 0; i < booksRetrieved.length; i++) {
-    const currentBook = booksRetrieved[i];
-    cartBooks.push(currentBook);
-    const li = document.createElement("li");
-    li.classList.add(
-      "navbar-item",
-      "mx-3",
-      "d-flex",
-      "justify-content-between",
-      "align-items-start",
-      "border-bottom",
-      "mt-2",
-      "pb-1"
-    );
-    li.style.display = "-webkit-box";
-    li.style.webkitBoxOrient = "vertical";
-    li.style.webkitLineClamp = "2";
-    li.style.overflow = "hidden";
-
-    const title = document.createElement("p");
-    title.classList.add("me-5", "mb-1");
-
-    const price = document.createElement("p");
-    title.innerText = currentBook.title;
-    price.innerText = currentBook.price;
-    const delBtn = document.createElement("i");
-    delBtn.classList.add("fa-solid", "fa-trash", "btn", "btn-danger", "ms-1");
-    delBtn.onclick = deleteCartBook;
-    li.append(title, price, delBtn);
-    cartList.appendChild(li);
-  }
-  console.log(booksRetrieved);
-};
-
-const buyElement = (event) => {
-  const cartList = document.getElementById("cart-list");
-  const title = event.currentTarget.parentElement.childNodes[0].innerText;
-  const price = event.currentTarget.parentElement.childNodes[1].innerText;
-  const book = new Book(title, price);
+/* funzione che aggiunge un oggetto in un container */
+const cartItemGen = (obj, container) => {
   const li = document.createElement("li");
   li.classList.add(
     "navbar-item",
@@ -67,27 +25,52 @@ const buyElement = (event) => {
   const titleLi = document.createElement("p");
   titleLi.classList.add("me-5", "mb-1");
   const priceLi = document.createElement("p");
-  titleLi.innerText = book.title;
-  priceLi.innerText = book.price;
+  titleLi.innerText = obj.title;
+  priceLi.innerText = obj.price;
   const delBtn = document.createElement("button");
   delBtn.classList.add("fa-solid", "fa-trash", "btn", "btn-danger", "ms-1");
   delBtn.onclick = deleteCartBook;
   li.append(titleLi, priceLi, delBtn);
 
-  cartList.appendChild(li);
+  container.appendChild(li);
 
-  cartBooks.push(book);
+  cartBooks.push(obj);
   const cart = document.getElementById("cart");
   cart.innerText = ` ${cartBooks.length}`;
 
   localStorage.setItem("Cart Books", JSON.stringify(cartBooks));
 };
 
+/* funzione che aggiunge un libro al carrello */
+const buyElement = (event) => {
+  const cartList = document.getElementById("cart-list");
+  const title = event.currentTarget.parentElement.childNodes[0].innerText;
+  const price = event.currentTarget.parentElement.childNodes[1].innerText;
+  const book = new Book(title, price);
+  cartItemGen(book, cartList);
+};
+
+/* funzione che genera il carrello partendo dal local storage */
+const cartGen = () => {
+  const books = localStorage.getItem("Cart Books");
+  const booksRetrieved = JSON.parse(books);
+  const cartList = document.getElementById("cart-list");
+  cartList.style.width = "300px";
+  const cart = document.getElementById("cart");
+  cart.innerText = ` ${booksRetrieved.length}`;
+  for (let i = 0; i < booksRetrieved.length; i++) {
+    const currentBook = booksRetrieved[i];
+    cartItemGen(currentBook, cartList);
+  }
+};
+
+/* funzione che scarta un libro */
 const deleteElement = (event) => {
   const currentCard = event.currentTarget.closest(".col-md-6");
   currentCard.remove();
 };
 
+/* funzione che toglie un libro dal carrello */
 const deleteCartBook = (event) => {
   const book = event.currentTarget.parentElement;
   book.remove();
@@ -96,7 +79,6 @@ const deleteCartBook = (event) => {
     const currentBook = cartBooks[i];
     if (currentBook.title === bookTitle) {
       cartBooks.splice(i, 1);
-      console.log(currentBook);
       break;
     }
   }
@@ -106,6 +88,7 @@ const deleteCartBook = (event) => {
   localStorage.setItem("Cart Books", JSON.stringify(cartBooks));
 };
 
+/* funzione che genera le card */
 const cardGen = (book, container) => {
   /* Creazione card */
   const col = document.createElement("div");
@@ -160,6 +143,7 @@ const cardGen = (book, container) => {
   container.appendChild(col);
 };
 
+/* Funzione che prende i libri dall'API e genera le card */
 const bookGen = () => {
   fetch("https://striveschool-api.herokuapp.com/books")
     .then((books) => {
@@ -176,7 +160,10 @@ const bookGen = () => {
         cardGen(currentBook, row);
       }
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+      bookGen();
+    });
 };
 
 window.addEventListener("DOMContentLoaded", bookGen);
